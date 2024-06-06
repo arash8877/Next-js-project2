3d3f57ee63db64ee77ccc3179b3c13b2ebde4e43
Arash <arash8877@gmail.com>
2024-06-06 10:36:52 +0200

    navigation in Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center gap-2 justify-between p-24">
       <h1>Welcome Home!</h1>
+      <Link href="/blog" style={{ background: "blue", color: "white" }}>
+        Go to Blog
+      </Link>
+      <Link href="/products" style={{ background: "blue", color: "white" }}>
+        Go to Products
+      </Link>
     </main>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,26 @@
+import Link from "next/link";
+
 export default function Products() {
   return (
     <>
+      <Link href="/" style={{ color: "white", background: "blue" }}>
+        Home
+      </Link>
       <h1>List of the products:</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>Product 3</Link>
+      </h2>
       <h2>.........</h2>
     </>
   );
 }
+
+
+// in Link there is a property called 'replace'. when you are in this link/product 
+// and click 'back', you redirect to the Home-page instead of product-list page.
