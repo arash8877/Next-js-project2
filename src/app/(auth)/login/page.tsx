ae1507b4abb616912a9772c48cb3d0c67179c0d3
Arash <arash8877@gmail.com>
2024-06-06 10:08:43 +0200

    title metadata

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,15 @@
 // to make a folder as route-groupe wrap the name by parentheses
 // then your route will be localhost:3000/Login
 
-export default function Login () {
-    return <h1>Login</h1>
+
+export const metadata = {
+  title: {
+    absolute: "Login",
+  },
+};
+
+export default function Login() {
+  return <h1>Login</h1>;
 }
+
+//// using title metadata - "absolute" key ; see rootLayout
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { Metadata } from "next";
+import { title } from "process";
+
+export const metadata = {
+  title: "Profile"
+}
 
 export default function  Profile()  {
   return <div>This is Profile page</div>;
 };
+
+
+
+
+// using title metadata - "template" key ; see rootLayout
 
 
 
